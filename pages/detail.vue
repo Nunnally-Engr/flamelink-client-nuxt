e8ca9fc44040ef25fb9f2c845708ae17889dfff2
井上奈々 <nana@diffeasy.com>
2019-01-27 11:18:34 +0900

    test commit

--- pages/detail.vue
+++ pages/detail.vue
@@ -1,35 +1,14 @@
 <template>
   <v-layout>
-    <v-flex 
-      xs12
-      sm8 
-      offset-sm3>
-      <v-card>
-        <v-card-title primary-title>
-          <div class="blog-box">
-            <div class="blog-date">投稿日：{{ detail.dataFormat }}</div>
-            <h3 
-              class="headline mb-0">{{ detail.title }}</h3>
-            <div 
-              class="blog-content" 
-              v-html="detail.content"/>
-          </div>
-        </v-card-title>
-      </v-card>
+    <v-flex text-xs-center>
+      <blockquote class="blockquote">
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
     </v-flex>
   </v-layout>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      detail: ''
-    }
-  },
-  async created() {
-    this.detail = await this.$route.query
-  }
-}
-</script>
-<style scoped>
-</style>
